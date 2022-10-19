3915cd738c997671fea719a0daea4c5a9d450c28
ilyin-fmf <ilyin@fmf.ru>
2022-10-19 20:11:22 +0300

    add api route, add controller House

---
+++ app/Http/Controllers/HouseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class HouseController extends Controller
+{
+    public function search(Request $request): JsonResponse
+    {
+        
+    }
+}
